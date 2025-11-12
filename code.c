213442104864ee26c6f98b1ef17b00dfe157a16d
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-11-12 22:18:32 +0700

    Create code.c

---
+++ code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int z1;
+  int z2;
+  int z3;
+  id = 18 % 10 + 1;
+  count_z = 19;
+  z1 = id + (count_z%id);
+  z2 = ((id*3)%count_z)+1;
+  z3 = (((id * 3) + id) % count_z)+1;
+  printf("%d, %d, %d\n", z1, z2, z3);
+  return 0;
+}
